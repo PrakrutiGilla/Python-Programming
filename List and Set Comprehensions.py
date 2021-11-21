69d8c5642911faed9bd8ae00c9f014f27c0b7ef1
PrakrutiGilla <84344700+PrakrutiGilla@users.noreply.github.com>
2021-11-21 11:44:06 +0530

    Update List and Set Comprehensions.py

--- List and Set Comprehensions.py
+++ List and Set Comprehensions.py
@@ -1,30 +1,30 @@
-"""Comprehensions in Python provide us with a short and concise way to construct new sequences (such as lists, set, dictionary etc.) """
+#Comprehensions in Python provide us with a short and concise way to construct new sequences (such as lists, set, dictionary etc.) 
 
 List Comprehension
 """syntax: [expression for item in iterable] (only for)
         [expression for item in iterable if conditional] (for, if)
         [exp if conditional else statement for item in iterable] (for, if-else) """
 
-#print([[i,j] for i in range(0,2) for j in range(0,2)])
+print([[i,j] for i in range(0,2) for j in range(0,2)])
 #o/p: [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-#x=2,y=2,z=2,n=3
-#print( [[i,j,k] for i in range(x) for j in range(y) for k in range(z) if(i+j!=3)])
+x=2,y=2,z=2,n=3
+print( [[i,j,k] for i in range(x) for j in range(y) for k in range(z) if(i+j!=3)])
 #o/p: [[0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 1, 0], [0, 1, 1], [0, 1, 2], [1, 0, 0], [1, 0, 1], [1, 0, 2], [1, 1, 0], [1, 1, 1], [1, 1, 2]]
 
-#print([x for x in range(0,6)])
+print([x for x in range(0,6)])
 #o/p: [0, 1, 2, 3, 4, 5]
 
-#print([i for i in "human"])
+print([i for i in "human"])
 #o/p: ['h', 'u', 'm', 'a', 'n']
 
-#print([i for i in range(0,100,10)])
+print([i for i in range(0,100,10)])
 #o/p: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 
-#print([i*i for i in range(0,10)])
+print([i*i for i in range(0,10)])
 #o/p: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-#print([x for x in range(100) if x % 3 == 0 if x % 5 == 0]) (nested if stmts)
+print([x for x in range(100) if x % 3 == 0 if x % 5 == 0]) (nested if stmts)
 #o/p: [0, 15, 30, 45, 60, 75, 90]
 
 Set Comprehensions
